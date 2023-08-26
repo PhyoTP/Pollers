5d7ac7cf8a8ad2eb59bbecc17b32345eab8cd293
PhyoTP <phyo.thet.pai@sji.edu.sg>
2023-08-26 10:15:40 +0800

    changed sample data

--- Pollers/Candidate+SampleData.swift
+++ Pollers/Candidate+SampleData.swift
@@ -11,9 +11,9 @@ import Foundation
 extension Candidate {
     
     static let sampleCandidates = [
-        Candidate(name: "Tan King Lian", age: 60, numberOfCats: 0, amountOfHair: 0.9),
-        Candidate(name: "Ng King Song", age: 50, numberOfCats: 1, amountOfHair: 0.7),
-        Candidate(name: "Tharcat", age: 55, numberOfCats: 3, amountOfHair: 0.1)
+        Candidate(name: "Comrade Potato", age: 60, numberOfCats: 0, amountOfHair: 0.9),
+        Candidate(name: "Definitely not YJ", age: 50, numberOfCats: 1, amountOfHair: 0.7),
+        Candidate(name: "Comrade Seen", age: 55, numberOfCats: 3, amountOfHair: 0.1)
     ]
     
 }
