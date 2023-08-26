692544813591a441aee33f412a44b66a8314a8e8
PhyoTP <phyo.thet.pai@sji.edu.sg>
2023-08-26 09:27:51 +0800

    Initial Commit

---
+++ Pollers/PollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PollersApp.swift
+//  Pollers
+//
+//  Created by T Krobot on 26/8/23.
+//
+
+import SwiftUI
+
+@main
+struct PollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
