0aa1734531cefe3a62f974d768c8a29ca3b90a42
PhyoTP <phyo.thet.pai@sji.edu.sg>
2023-08-26 11:21:14 +0800

    vote saumil

--- Pollers/CommunismView.swift
+++ Pollers/CommunismView.swift
@@ -1,13 +1,29 @@
 import SwiftUI
 
 struct CommunismView: View {
+    @State private var population = 10000
     var body: some View {
         ZStack{
             LinearGradient(colors: [.black,.red,.yellow], startPoint: .topLeading, endPoint: .bottomTrailing)
                 .ignoresSafeArea()
             VStack{
                 Image("saumil")
-                
+                HStack{
+                    Button{
+                        population += 1
+                    }label: {
+                        Text("Vote Saumil")
+                            .font(.title)
+                    }
+                    Button{
+                        population += 1
+                    }label: {
+                        Text("Vote Saumil")
+                            .font(.title)
+                    }
+                    
+                }
+                Text("\(population) comrades")
             }
         }
         
